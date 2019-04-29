99ae7643b58b6e7861a304d0d548aaf0f1e21de2
Joy Lin <vvlnote@hotmail.com>
2019-04-29 19:51:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    erb :user_input
+  end
+  
+  post '/piglatinize' do
+    puts params
+    erb :piglatinize
+  end
+  
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,25 @@
+class PigLatinizer
+  
+  def piglatinize(text)
+    str_arr = text.split
+    new_str_arr = []
+    for x in 0...str_arr.size do
+      index = str_arr[x].downcase.index(/[aeiou]/)
+      new_str = ''
+      if index == 0
+        new_str = str_arr[x] + 'way'
+        new_str_arr << new_str
+      elsif index > 0
+        #binding.pry
+        new_str = str_arr[x][index..-1] + str_arr[x][0..index-1] + "ay"
+        new_str_arr << new_str
+        #binding.pry
+      else
+        new_str = str_arr[x] + "ay"
+        new_str_arr << new_str
+      end
+      #binding.pry
+    end
+    new_str_arr.join(' ')
+  end
+end
