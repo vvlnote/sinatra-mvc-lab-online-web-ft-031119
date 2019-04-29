08f6c2f3ade2b8552edf7a125329d97d25723d44
Joy Lin <vvlnote@hotmail.com>
2019-04-29 20:14:09 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,9 +1,9 @@
 class PigLatinizer
   
   def piglatinize(text)
-    puts "text = #{text}"
+
     str_arr = text.split
-    binding.pry
+
     new_str_arr = []
     for x in 0...str_arr.size do
       index = str_arr[x].downcase.index(/[aeiou]/)
@@ -12,15 +12,12 @@ class PigLatinizer
         new_str = str_arr[x] + 'way'
         new_str_arr << new_str
       elsif index > 0
-        #binding.pry
         new_str = str_arr[x][index..-1] + str_arr[x][0..index-1] + "ay"
         new_str_arr << new_str
-        #binding.pry
       else
         new_str = str_arr[x] + "ay"
         new_str_arr << new_str
       end
-      #binding.pry
     end
     new_str_arr.join(' ')
   end
